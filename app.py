4d44e5c35cae3f66adfc78864a262f767abee8b8
Echoic88 <caulfield.bd@outlook.com>
2019-10-15 20:41:26 +0100

    Add basic routing to test flask and mongoDb connections

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+ 
+app = Flask(__name__)
+app.config["MONGODB_NAME"] = "guitarReview"
+app.config["MONGO_URI"] = os.getenv("mongoURI")
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def test():
+    
+    return render_template("test2.html", guitars=mongo.db.guitars.find())
+
+
+if __name__ == "__main__":
+    app.run(host=os.getenv("IP"), port=(os.getenv("PORT")), debug=True)
