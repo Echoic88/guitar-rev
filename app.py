eb60f8037bc96b21048b2a540187cadba02256a0
Echoic88 <caulfield.bd@outlook.com>
2019-10-16 12:56:13 +0100

    Guitar form submits user guitar data to DB

--- app.py
+++ app.py
@@ -12,31 +12,48 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/home")
 def home():
+    """ 
+    Navigate to home page
+    """
     return render_template("base.html")
 
 
 @app.route("/guitars")
 def guitars():
+    """
+    Navigate to the list of user guitars
+    """
     guitars = mongo.db.guitars
     return render_template("guitars.html", guitars=guitars.find())
 
 
-@app.route("/input_guitar")
-def input_guitar():
+@app.route("/guitars_form")
+def guitars_form():
+    """
+    When "Tell Us About Your Guitar" button is
+    pressed navigate to guitars form 
+    """
     return render_template("guitars-form.html")
 
 
-@app.route("/form_submit")
-def form_submit():
-    return render_template("base.html")
+@app.route("/input_guitar", methods=["POST"])
+def input_guitar():
+    """
+    Insert details from the guitars_form to a new
+    DB entry in guitars collection
+    """
+    guitars=mongo.db.guitars
+    guitars.insert_one(request.form.to_dict())
+    return render_template("guitars.html")
+
 
 @app.route("/poll")
 def poll():
+    """
+    Navigate to Poll page to vote on exciting guitar
+    """
     guitars = mongo.db.guitars
     return render_template("poll.html", guitars=guitars.find())
-
-
-
 
 
 if __name__ == "__main__":
