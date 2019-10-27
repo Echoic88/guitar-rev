79f49f5e737eba6d62f210b738f4ca5c346424c3
Echoic88 <caulfield.bd@outlook.com>
2019-10-27 15:14:47 +0000

    Draw basic graph on Poll Results page

--- static/js/script.js
+++ static/js/script.js
@@ -1,48 +1,45 @@
 $(document).ready(function () {
     $('.sidenav').sidenav();
 
-    //let results = $("#resList").text();
-    /*
-    document.getElementById("input").css("background-color", "red")
-
-    //Make graph for poll results - dummy values below
-    var results = [{
-            "guitar_name": "strat",
-            "votes": 12
-        },
-        {
-            "guitar_name": "lesPaul",
-            "votes": 5
-        },
-        {
-            "guitar_name": "xguitar",
-            "votes": 7
-        },
-    ];
+    // Force reload of script.js to override caching
+    // Stack Overflow
+    //https://stackoverflow.com/questions/50276601/avoid-javascript-css-caching
+    
+    let resourcePath = 'static/js/script.js';
+    document.getElementById('myScript').src = resourcePath + '?v=' + Date.now();
 
 
-    //From CI Lesson
-    let ndx = crossfilter(results);
-    let guitar_dim = ndx.dimension(dc.pluck("guitar_name"));
-    let votes_dim = guitar_dim.group().reduceSum(dc.pluck("votes"));
+        let res = JSON.parse($("#resList").text());
+        //Make graph for poll results
+        
+        window.onload = makeGraph(res);
 
-    dc.barChart("#pollChart")
-        .width(300)
-        .height(150)
-        .margins({
-            top: 10,
-            right: 50,
-            bottom: 30,
-            left: 50
-        })
-        .dimension(guitar_dim)
-        .group(votes_dim)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .xAxisLabel("votes")
-        .yAxis().ticks(4);
+        function makeGraph(results) {
+            console.log(results);
+            
+            //From CI Lesson
+            let ndx = crossfilter(results);
+            let guitar_dim = ndx.dimension(dc.pluck("_id"));
+            let votes_dim = guitar_dim.group().reduceSum(dc.pluck("number_of_votes"));
 
-    dc.renderAll();
-    */
+            dc.barChart("#pollChart")
+                .width(300)
+                .height(150)
+                .margins({
+                    top: 10,
+                    right: 50,
+                    bottom: 30,
+                    left: 50
+                })
+                .dimension(guitar_dim)
+                .group(votes_dim)
+                .transitionDuration(500)
+                .x(d3.scale.ordinal())
+                .xUnits(dc.units.ordinal)
+                .xAxisLabel("votes")
+                .yAxis().ticks(4);
+
+            dc.renderAll();
+        }
+     
 });
